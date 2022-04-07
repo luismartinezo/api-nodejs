078e3a57ba0fa5440c626a42a80e449d45ab1a20
Luis Martinez <lmartinezocoro@gmail.com>
2022-04-07 00:05:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NodejsApi
+Creación de una API de mantenimiento de comentarios para los fines de iniciar sesión y crear comentarios
---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
